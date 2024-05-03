f17730db33b32b546526c58a1c1cbfd59f4abf44
G-Maita97 <gustavoalternativox@gmail.com>
2024-05-03 12:41:23 -0500

    Task1 Never fail!

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -79,18 +79,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(inst.number, 89)
 
     def test_datetime_attributes(self):
-        """Test that two BaseModel instances have different datetime objects
-        and that upon creation have identical updated_at and created_at
-        value."""
+        """Comprueba que dos instancias de BaseModel tengan objetos datetime
+        diferentes y que al crearse tengan valores idénticos para created_at y
+        updated_at."""
         tic = datetime.now()
         inst1 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
+        # Imprimir valores para depuración
+        print(f'tic: {tic}, created_at inst1: {inst1.created_at}, toc: {toc}')
+        self.assertTrue(tic <= inst1.created_at >= toc)
+        time.sleep(2)  # Esperar 1 milisegundo antes de crear inst2
         tic = datetime.now()
         inst2 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
+        # Imprimir valores para depuración
+        print(f'tic: {tic}, created_at inst2: {inst2.created_at}, toc: {toc}')
+        self.assertTrue(tic <= inst2.created_at >= toc)
+
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
